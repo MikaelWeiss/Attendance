77ef40d1cad4e4e044ae5ae2227a234bd37817b4
Mikael Weiss <Campingmikael@icloud.com>
2020-06-03 16:11:47 -0600

    Fixed up a few things

--- Attendance/AttendanceView.swift
+++ Attendance/AttendanceView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct AttendanceView: View {
     @State private var people: [Person] = [Person("Mikael"), Person("Collin")]
     @State private var addingPerson = ""
+    @State private var keyboardIsActive = false
     
     var body: some View {
         NavigationView {
@@ -40,6 +41,12 @@ struct AttendanceView: View {
                     .frame(height: 600)
                     .foregroundColor(Color.white.opacity(0.0001))
             }
+//            .onTapGesture {
+//                print(self.keyboardIsActive)
+//                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+//                self.addingPerson = ""
+//                self.keyboardIsActive = false
+//            }
 //MARK: - NavBar Set-Up
             .navigationBarTitle("Attendance")
             .navigationBarItems(trailing:
@@ -90,7 +97,7 @@ struct PersonCell: View {
             
             Text(person.name)
             Rectangle()
-                .fill(Color.black.opacity(0.01))
+                .foregroundColor(Color.white.opacity(0.0001))
         }
     }
     init(for person: Person) {
@@ -99,7 +106,7 @@ struct PersonCell: View {
 }
 
 //MARK: - Structures
-struct Person: Identifiable {
+struct Person: Identifiable, Codable {
     let id = UUID()
     var name: String = ""
     var isPresent: Bool = false
@@ -108,33 +115,45 @@ struct Person: Identifiable {
         self.name = name
     }
 }
-//
-//struct People: Codable {
-//    var list: [Person]
-//}
-//
-//
-////MARK: - Functions
-//
-//func setPeople(people: [Person]) {
-//    let encoder = JSONEncoder()
-//    if let encoded = try? encoder.encode(people) {
-//        let defaults = UserDefaults.standard
-//        defaults.set(encoded, forKey: "SavedPeople")
-//    }
-//}
-//
-//func getPeople() -> [Person] {
-//    let defaults = UserDefaults.standard
-//    if let savedPeople = defaults.object(forKey: "SavedPeople") as? Data {
-//        let decoder = JSONDecoder()
-//        if let loadedPeople = try? decoder.decode(People.self, from: savedPeople) {
-//            var people: [Person] = []
-//            for person in loadedPeople.list {
-//                people.append(person)
-//            }
-//            return people
-//        }
-//    }
-//    return [Person("Johny Appleseed")]
-//}
+
+struct People: Codable {
+    var list: [Person]
+}
+
+
+//MARK: - Functions
+
+func setPeople(people: [Person]) {
+    let encoder = JSONEncoder()
+    if let encoded = try? encoder.encode(people) {
+        let defaults = UserDefaults.standard
+        defaults.set(encoded, forKey: "SavedPeople")
+    }
+}
+
+func getPeople() -> [Person] {
+    let defaults = UserDefaults.standard
+    if let savedPeople = defaults.object(forKey: "SavedPeople") as? Data {
+        let decoder = JSONDecoder()
+        if let loadedPeople = try? decoder.decode(People.self, from: savedPeople) {
+            var people: [Person] = []
+            for person in loadedPeople.list {
+                people.append(person)
+            }
+            return people
+        }
+    }
+    return [Person("Johny Appleseed")]
+}
+
+//MARK: - Extentions
+
+extension View {
+   func `if`<Content: View>(_ conditional: Bool, content: (Self) -> Content) -> some View {
+        if conditional {
+            return AnyView(content(self))
+        } else {
+            return AnyView(self)
+        }
+    }
+}
